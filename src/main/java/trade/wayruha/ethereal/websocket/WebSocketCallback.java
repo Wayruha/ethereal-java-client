2e31223f7bb8c5f059f2f884bfe33c4cbf01efec
Kristex95 <kristex95@gmail.com>
2026-01-14 02:35:10 +0200

    feat: added WebSocketCallback type references for orderInfo and orderFill

--- src/main/java/trade/wayruha/ethereal/websocket/WebSocketCallback.java
+++ src/main/java/trade/wayruha/ethereal/websocket/WebSocketCallback.java
@@ -2,10 +2,15 @@ package trade.wayruha.ethereal.websocket;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import okhttp3.Response;
+import trade.wayruha.ethereal.dto.response.OrderFill;
+import trade.wayruha.ethereal.dto.response.OrderInfo;
+import trade.wayruha.ethereal.dto.response.PageableResponse;
 import trade.wayruha.ethereal.dto.wsresponse.WSBookDepthUpdate;
 
 public interface WebSocketCallback<T> {
     TypeReference<WSBookDepthUpdate> ORDER_BOOK_UPDATE_TYPE = new TypeReference<>() {};
+    TypeReference<PageableResponse<OrderInfo>> ORDER_INFO_UPDATE_TYPE = new TypeReference<>() {};
+    TypeReference<PageableResponse<OrderFill>> ORDER_FILL_UPDATE_TYPE = new TypeReference<>() {};
 
     /**
      * Be called when the request successful.
